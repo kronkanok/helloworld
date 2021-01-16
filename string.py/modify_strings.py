60e9802468f6eb406ee5e4fb14b6c120dd95a1ad
kronkanok <kronkanok.h@ku.th>
2021-01-16 11:45:47 +0700

    add new seven file

---
+++ string.py/modify_strings.py
@@ -0,0 +1,8 @@
+string = " Hello, World! "
+
+print(string.upper()) #Output : "Hello, World!"
+print(string.lower()) #Output : "hello, world!"
+print(string.strip()) #Output : "Hello, World!" #แก้ไขคำที่error
+print(string.replace("H","J")) #Output : "Jello World! " แก้ไขคำ
+print(string.split(",")) #Output :  ['Hello', ' World! '] ตัดหัวท้ายทิ้ง
+print(len(string))  #15
